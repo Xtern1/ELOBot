6d547d8718fd56d64aea698681859bff5f45cfbc
Xtern1 <30741082+Xtern1@users.noreply.github.com>
2018-11-12 14:55:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Discord
 const Discord = require('discord.js');
-const app  = new Discord.Client();
+const app = new Discord.Client();
 
+const framework = require('./resources/framework.js')
 
 app.login("TOKEN")
